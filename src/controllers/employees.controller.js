a9d8d13469ead768c43d8694e2100381db89f719
Kevin huelgos <kahuelgos@soy.sena.edu.co>
2023-08-31 22:26:14 -0500

    actualizacion

--- src/controllers/employees.controller.js
+++ src/controllers/employees.controller.js
@@ -1,11 +1,32 @@
 import { pool } from '../db.js'
 
 
-export const getEmployees = (req, res)=> res.send('Obteniendo Empleados')
+export const getEmployees = async(req, res)=> {
+    const [rows] = await pool.query("SELECT * FROM employee")
+    res.json(rows)
+}
+export const getEmployee = async(req, res)=> {
+    const [rows] = await pool.query("SELECT * FROM employee WHERE id = ?",[req.params.id])
+    if(rows.length <= 0) return res.status(404).json({
+        message: "No existe un registro con ese id"
+    })
+    res.json(rows[0])
+}
 export const createEmployees = async(req, res)=>{ 
     const {name,salary} = req.body
     const [rows] = await pool.query("INSERT INTO employee (name,salary) VALUES (?,?)",[name,salary])
-    res.send({rows})
+    res.send({
+        id:rows.insertId,
+        name, 
+        salary
+    })
 }
 export const updateEmployees = (req, res)=> res.send('Actualizando emplados')
-export const deleteEmployees = (req, res)=> res.send('Eliminando emplados')
+
+export const deleteEmployees = async(req, res)=> {
+    const [result] = await pool.query("DELETE FROM employee WHERE id = ?",[req.params.id])
+    if (result.affectedRows <=0) return res.status(404).json ({
+        message:"Empleado no encontrado"
+    })
+    res.send("Empleado eliminado")
+}
--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express"
-import { getEmployees, createEmployees, updateEmployees, deleteEmployees } from "../controllers/employees.controller.js"
+import { getEmployees, createEmployees, updateEmployees, deleteEmployees, getEmployee } from "../controllers/employees.controller.js"
 
 
 const router = Router()
 
 router.get('/employees',getEmployees)
+router.get('/employees/:id',getEmployee)
 router.post('/employees',createEmployees)
 router.put('/employees',updateEmployees)
-router.delete('/employees',deleteEmployees)
+router.delete('/employees/:id',deleteEmployees)
 
 export default router
