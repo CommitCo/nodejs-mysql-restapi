2d854160ffe5599d4a2280072027970390328942
CommitCo <diego.cortes14@misena.edu.co>
2023-09-02 21:33:27 -0500

    ultimo ajuste

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { getEmployees, createEmployees, updateEmployees, deleteEmployees, getEmployee } from "../controllers/employees.controller.js"
+import { getEmployees, createEmployees, updateEmployees, deleteEmployees, getEmployee, updateEmployee } from "../controllers/employees.controller.js"
 
 
 const router = Router()
@@ -7,7 +7,8 @@ const router = Router()
 router.get('/employees',getEmployees)
 router.get('/employees/:id',getEmployee)
 router.post('/employees',createEmployees)
-router.put('/employees',updateEmployees)
+router.put('/employees/:id',updateEmployees)
+router.patch('/employees/:id',updateEmployee)
 router.delete('/employees/:id',deleteEmployees)
 
 export default router
