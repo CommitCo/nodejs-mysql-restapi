c69b2d5508c1dbb6156e38c66983017a9ad9e6db
Kevin huelgos <kahuelgos@soy.sena.edu.co>
2023-09-07 22:15:57 -0500

    ultimo

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import express from "express"
-import employeesRoutes from "./routes/employees.routes.js"
-import indexRoutes from "./routes/index.routes.js"
+import {PORT} from'./config.js'
+import app from "./app.js"
 
-const app = express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use("/api",employeesRoutes)
-
-
-app.listen(3000)
-
-console.log("servidor en puerto http://localhost:3000");
+app.listen(PORT)
+console.log("servidor en puerto http://localhost:3000")
